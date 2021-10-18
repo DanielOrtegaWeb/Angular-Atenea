1745d1b2a44a58522583b0591d7719515fddedd7
daniel ortega <danielfortega92@gmail.com>
2021-10-18 19:27:10 -0400

    ANGULAR-ATENEA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEstudiantesComponent } from './components/list-estudiantes/list-estudiantes.component';
+import { CreateEstudiantesComponent } from './components/create-estudiantes/create-estudiantes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEstudiantesComponent,
+    CreateEstudiantesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
